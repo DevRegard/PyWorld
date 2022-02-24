83cf487b1b6868a2fc77a49aa2c367ecb9344fa6
UTR_ADP <utr160626@gmail.com>
2022-02-24 18:12:49 +0900

    Mac_Dev Python Test

--- jump_to/02_control_statement/While.py
+++ jump_to/02_control_statement/While.py
@@ -2,3 +2,10 @@ i = 10
 while i >= 0:
     print(f"{i} \t {i**i}")
     i = i - 1
+
+treeHit = 0
+while treeHit < 10:
+    treeHit = treeHit + 1
+    print("나무를 %d번 칩니다." % treeHit)
+    if treeHit == 10:
+        print("나무 죽다.")
